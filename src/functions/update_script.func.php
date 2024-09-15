4f6e3bec57ec35850d86fe7165df2f48098236b4
MrCl0wnLab <0x27null@gmail.com>
2024-09-15 02:17:35 -0300

    update system using git commands / inurlbr --update

--- src/functions/update_script.func.php
+++ src/functions/update_script.func.php
@@ -1,31 +1,35 @@
 <?php
 
+function __msg_update_error(){
+    echo system("command clear") . __bannerLogo();
+    echo "{$_SESSION["c1"]}[  x  ] {$_SESSION["c16"]}FAILURE TO SERVER!\n{$_SESSION["c0"]}";
+}
+
 function __update() {
 
     echo __bannerLogo();
 
-    echo "{$_SESSION["c1"]}__[  !  ] {$_SESSION["c16"]}WANT TO MAKE UPDATE SCRIPT\n{$_SESSION["c0"]}";
-    echo "{$_SESSION["c1"]}__[  !  ] {$_SESSION["c16"]}This can modify the current script\n{$_SESSION["c0"]}";
-    echo "{$_SESSION["c1"]}__[  !  ] {$_SESSION["c16"]}ARE YOU SURE ? (y \ n): {$_SESSION["c0"]}";
+    echo "{$_SESSION["c1"]}[  !  ] {$_SESSION["c16"]}WANT TO MAKE UPDATE SCRIPT\n{$_SESSION["c0"]}";
+    echo "{$_SESSION["c1"]}[  !  ] {$_SESSION["c16"]}This can modify the current script\n{$_SESSION["c0"]}";
+    echo "{$_SESSION["c1"]}[  !  ] {$_SESSION["c16"]}ARE YOU SURE ? (y \ n): {$_SESSION["c0"]}";
 
     if (trim(fgets(STDIN)) == 'y'):
-
-        $resultado = __request_info("https://raw.githubusercontent.com/MrCl0wnLab/SCANNER-INURLBR/master/inurlbr", $_SESSION["config"]["proxy"], NULL);
-
-        if (__not_empty($resultado['corpo'])):
-
-            unlink('inurlbr');
-            $varf = fopen('inurlbr', 'a');
-            fwrite($varf, $resultado['corpo']);
-            fclose($varf);
-            chmod('inurlbr', 0777);
-            echo "\nUPDATE DONE WITH SUCCESS!\n";
-            sleep(3);
-            system("chmod +x inurlbr | ./inurlbr");
-            exit();
+        $cmd_status = "git -C {$_SESSION['config']['pwd']}/../../ status | grep 'Your branch is behind'";
+        echo "{$_SESSION["c1"]}[  !  ] {$_SESSION["c16"]}{$cmd_status}\n{$_SESSION["c0"]}";
+        exec($cmd_status, $output_status, $return_status);
+        if(!$return_status):
+            echo "{$_SESSION["c1"]}[  !  ] {$_SESSION["c16"]}UPDATE...\n{$_SESSION["c0"]}";
+            print_r($output_status);
+            $cmd_pull = "git -C {$_SESSION['config']['pwd']}/../../ pull origin master";
+            exec($cmd_pull, $output_pull, $return_pull);
+            if(!$return_status):
+                echo "{$_SESSION["c1"]}[  +  ] {$_SESSION["c16"]}OK\n{$_SESSION["c0"]}";
+                print_r($output_pull);
+            else:
+                __msg_update_error();
+            endif;
         else:
-            echo system("command clear") . __bannerLogo();
-            echo "{$_SESSION["c1"]}__[ x ] {$_SESSION["c16"]}FAILURE TO SERVER!\n{$_SESSION["c0"]}";
+            __msg_update_error();
         endif;
     endif;
 }
