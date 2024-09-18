fb2b98eca750d63e15209563809aad18825e23ac
Mr. Cl0wn - H4ck1ng C0d3r <mrcl0wnlab@gmail.com>
2024-09-18 16:06:39 -0300

    Melhor validação de Regex Personalizada

--- src/functions/extract_reg_custom.func.php
+++ src/functions/extract_reg_custom.func.php
@@ -1,23 +1,25 @@
 <?php
 
-function __extractRegCustom($html, $url_) {
-    if ($html and $url_):
-    $matches = NULL;
+function __extractRegCustom($html, $url) {
+    if (__not_empty($html) and __not_empty($url)):
+        $out_matches = NULL;
         __plus();
-        preg_match_all("#\b{$_SESSION['config']['regexp-filter']}#i", $html, $matches);
-        echo "{$_SESSION["c1"]}{$_SESSION['config']['line']}{$_SESSION["c0"]}\n";
-        echo "{$_SESSION["c1"]}[ INF ][URL REGEX CUSTOM] {$_SESSION["c0"]}=>{$_SESSION["c9"]} {$url_} {$_SESSION["c0"]}\n";
-        $matches_ = array_filter(array_unique(array_unique($matches[0])));
-        foreach ($matches_ as $valor):
-            if (__not_empty($valor)):
-                echo "{$_SESSION["c1"]}[  +  ]{$_SESSION["c0"]}[\033[01;31m {$_SESSION['config']['cont_valores']} {$_SESSION["c0"]}] {$valor}\n";
-                $_SESSION["config"]["resultado_valores"].="{$valor}\n";
+        preg_match_all("#\b{$_SESSION['config']['regexp-filter']}#i", $html, $out_matches);
+        echo "{$_SESSION["c1"]}{$_SESSION['config']['line']}{$_SESSION["c0"]}".PHP_EOL;
+        echo "{$_SESSION["c1"]}[ INF ][URL REGEX CUSTOM] {$_SESSION["c0"]}=>{$_SESSION["c9"]} {$url} {$_SESSION["c0"]}".PHP_EOL;
+        $out_matches_unique = array_filter(array_unique(array_unique($out_matches[0])));
+        if(__not_empty($out_matches_unique)):
+            foreach ($out_matches_unique as $valor):
+                if (__not_empty($valor)):
+                    echo "{$_SESSION["c1"]}[  +  ]{$_SESSION["c0"]}[\033[01;31m {$_SESSION['config']['cont_valores']} {$_SESSION["c0"]}] {$valor}".PHP_EOL;
+                    $_SESSION["config"]["resultado_valores"].=$valor.PHP_EOL;
+                    __plus();
+                    __saveValue($_SESSION["config"]["arquivo_output"], $valor);
+                    $_SESSION['config']['cont_valores'] ++;
+                endif;
                 __plus();
-                __saveValue($_SESSION["config"]["arquivo_output"], $valor);
-                $_SESSION['config']['cont_valores'] ++;
-            endif;
-            __plus();
-        endforeach;
-        __timeSec('delay', "\n");
+            endforeach;
+            __timeSec('delay', "\n");
+        endif;
     endif;
 }
