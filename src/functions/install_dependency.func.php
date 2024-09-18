52bf607afb952cf31f590cbbfaace686556155c4
Mr. Cl0wn - H4ck1ng C0d3r <mrcl0wnlab@gmail.com>
2024-09-18 16:05:46 -0300

    Referência  apt-get ao PHP8

--- src/functions/install_dependency.func.php
+++ src/functions/install_dependency.func.php
@@ -4,7 +4,7 @@ function __installDepencia() {
 
     echo "\n{$_SESSION["c15"]}|_[ * ]__{$_SESSION["c0"]}\n";
     echo "         {$_SESSION["c15"]}|[EXTERNAL COMMAND INSTALLING PREMISES ]:: {$_SESSION["c11"]}\n";
-    $dados = system("sudo apt-get install curl libcurl3 libcurl3-dev php7 php7-cli php7-curl xterm tor", $dados) . __plus();
+    $dados = system("sudo apt-get install curl libcurl3 libcurl3-dev php8 php8-cli php8-curl xterm tor", $dados) . __plus();
     sleep(1) . __plus();
     echo "{$_SESSION["c0"]}";
     if (empty($dados)):
