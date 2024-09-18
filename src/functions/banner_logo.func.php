74e06a0ed63f9257beef9a9948a09df381d2b74f
Mr. Cl0wn - H4ck1ng C0d3r <mrcl0wnlab@gmail.com>
2024-09-18 16:07:10 -0300

    Retorn and PHP_EOL

--- src/functions/banner_logo.func.php
+++ src/functions/banner_logo.func.php
@@ -16,7 +16,7 @@ function __bannerLogo() {
     {$_SESSION["c16"]} ░              ░       ░           ░            ░  ░  {$_SESSION["c1"]}  ░            ░     {$_SESSION["c0"]}{$_SESSION["c1"]}{$_SESSION['config']['version_script']}
     {$_SESSION["c0"]}"; 
                                                    
-return print(!is_null($_SESSION['config']['no-banner']) ? NULL : system("clear") . $banner."
+return !is_null($_SESSION['config']['no-banner']) ? NULL : system("clear") . $banner."
 {$_SESSION["c12"]} Current IPinfo Token::[ {$_SESSION["c0"]}{$_SESSION["c1"]}" . $_SESSION['config']['token_ipinfo'][0] . " {$_SESSION["c12"]}]{$_SESSION["c0"]}
 {$_SESSION["c12"]} Current PHP version::[ {$_SESSION["c0"]}{$_SESSION["c1"]}" . phpversion() . " {$_SESSION["c12"]}]{$_SESSION["c0"]}
 {$_SESSION["c12"]} Current Script Owner::[ {$_SESSION["c0"]}{$_SESSION["c1"]}" . get_current_user() . " {$_SESSION["c16"]}]{$_SESSION["c0"]}
@@ -24,5 +24,5 @@ return print(!is_null($_SESSION['config']['no-banner']) ? NULL : system("clear")
 {$_SESSION["c12"]} Current PWD::[ {$_SESSION["c0"]}{$_SESSION["c1"]}" . getcwd() . " {$_SESSION["c12"]}]{$_SESSION["c0"]}
 {$_SESSION["c2"]} Help: inurlbr --help{$_SESSION["c0"]}
  --------------------------------------------------------------------------------------------------------------{$_SESSION["c0"]}
-".PHP_EOL); 
+".PHP_EOL; 
 }
