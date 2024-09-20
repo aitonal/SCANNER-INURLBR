24d4e8f63a892b2d506cba955b72e2258f881d22
Mr. Cl0wn - H4ck1ng C0d3r <mrcl0wnlab@gmail.com>
2024-09-20 09:09:42 -0300

    Definição de retorno e formatação de msg

--- src/functions/save_value.func.php
+++ src/functions/save_value.func.php
@@ -1,11 +1,11 @@
 <?php
 
-function __saveValue($arquivo, $valor, $op = NULL) {
+function __saveValue($arquivo, $valor, $op = NULL): void {
 
     $path = !__not_empty($_SESSION['config']['save-as']) ? $_SESSION['config']['out_put_paste'] : NULL;
     
     if($op == 1):
-        echo PHP_EOL."{$_SESSION["c1"]}[  !  ]{$_SESSION["c1"]} VALUE SAVED IN THE FILE::{$_SESSION["c9"]} {$arquivo}{$_SESSION["c0"]}";
+        echo PHP_EOL."{$_SESSION["c1"]}[  !  ]{$_SESSION["c1"]}  VALUE SAVED IN THE FILE::{$_SESSION["c9"]} {$arquivo}{$_SESSION["c0"]}";
     endif;
 
     file_put_contents(($op == 2) ? $arquivo : $path . $arquivo, $valor.PHP_EOL, FILE_APPEND);
