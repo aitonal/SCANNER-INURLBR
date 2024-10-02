5110b1368cc9c9389858e3c4eb1aa9fbbea7eec2
MrCl0wnLab <0x27null@gmail.com>
2024-10-02 18:58:20 -0300

    Correção de bug sobre comando --update

--- src/functions/update_script.func.php
+++ src/functions/update_script.func.php
@@ -2,8 +2,9 @@
 
 function __msg_update_error(): void{
     $cor = $GLOBALS['COR']; 
-    echo system("command clear");
-    echo "{$cor->whit}[  x  ] {$cor->red2}FAILURE TO SERVER!{$cor->end}", PHP_EOL;
+    echo __bannerLogo();
+    echo "{$cor->whit}[  x  ] {$cor->red2}Failure to server!{$cor->end}", PHP_EOL;
+    __plus();
 }
 
 function __check_update(): array{
@@ -11,30 +12,38 @@ function __check_update(): array{
     $cmd_status = "git -C {$_SESSION['config']['pwd']}/../../ status | grep 'Your branch is behind'";
     echo "{$cor->whit}[  !  ] {$cor->red2}{$cmd_status}{$cor->end}", PHP_EOL;
     exec($cmd_status, $output_status, $return_status);
+    __plus();
     return [$return_status, $output_status];
 }
 
 function __update(): void {
     $cor = $GLOBALS['COR']; 
-    echo "{$cor->whit}[  !  ] {$cor->red2}WANT TO MAKE UPDATE SCRIPT{$cor->end}", PHP_EOL;
+    echo __bannerLogo();
+    __plus();
+    echo "{$cor->whit}[  !  ] {$cor->red2}Want to make update script{$cor->end}", PHP_EOL;
     echo "{$cor->whit}[  !  ] {$cor->red2}This can modify the current script{$cor->end}", PHP_EOL;
-    echo "{$cor->whit}[  !  ] {$cor->red2}ARE YOU SURE ? (y \ n): {$cor->end}";
+    echo "{$cor->whit}[  !  ] {$cor->red2}Are you sure ? (y \ n): {$cor->end}";
+    __plus();
 
     if (trim(fgets(STDIN)) == 'y'):
         [$return_status,$output_status] = __check_update();
-        if(!$return_status):
+        if(__not_empty($return_status)):
             echo "{$cor->whit}[  !  ] {$cor->red2}UPDATE...{$cor->end}", PHP_EOL;
             print_r($output_status);
+            __plus();
             $cmd_pull = "git -C {$_SESSION['config']['pwd']}/../../ pull origin master";
             exec($cmd_pull, $output_pull, $return_pull);
             if(!$return_status):
                 echo "{$cor->whit}[  +  ] {$cor->red2}OK{$cor->end}", PHP_EOL;
                 print_r($output_pull);
+                __plus();
             else:
                 __msg_update_error();
+                __plus();
             endif;
         else:
             __msg_update_error();
+            __plus();
         endif;
     endif;
 }
