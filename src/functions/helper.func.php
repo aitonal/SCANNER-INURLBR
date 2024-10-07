0806eeba6b8521e9b05abc3632d5fd323478e3fa
Mr. Cl0wn - H4ck1ng C0d3r <mrcl0wnlab@gmail.com>
2024-10-06 23:32:04 -0300

    Melhoria de function __filterHostname

--- src/functions/helper.func.php
+++ src/functions/helper.func.php
@@ -265,7 +265,10 @@ function __filterHostname($url) {
         $target = null;
         //#\b((((ht|f)tps?://*)|(www|ftp)\.)[a-zA-Z0-9-\.]+)#i - 1.0
         preg_match_all('@^(?:(ht|f)tps?://*)?([^/]+)@i', $url, $target);
-        return str_replace("/", '', str_replace("ftps:", '', str_replace("ftp:", '', str_replace("https:", '', str_replace("http:", '', $target[0][0])))));
+        $replace = [
+            '/', 'ftps:', 'ftp:','src="', 'https:', 'http:'
+        ];
+       return str_replace($replace, '', $target[0][0]);
     endif;
 }
 
